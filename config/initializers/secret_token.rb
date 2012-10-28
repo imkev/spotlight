2dde02f116986ab85a542c1a31ef79c503c0267a
imkev <leyen.luo@gmail.com>
2012-10-28 15:43:45 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spotlight::Application.config.secret_token = 'f3b8d97cd5bfff29ea934d4d2eec076dab83e4b01fcc07028ff4025168fd93aa6954d3144c64b5bf884f3338c9a093b850b1a76dfaea6e0e205ddbf1c29b4a1f'
